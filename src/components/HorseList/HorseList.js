93d60c5d6f9e7b760ee964fec1883d0a4feb4f52
Ntuthuko Mthiyane <ntuthuko.mthiyane@SAMAC001.Home>
2018-02-14 21:44:43 +0200

    made the horses random

--- src/components/HorseList/HorseList.js
+++ src/components/HorseList/HorseList.js
@@ -28,12 +28,22 @@ export class Horse extends React.Component{
 export class HorseList extends React.Component{
     
     populateHorses(){
-        var racingHorses = [];
-        //Itarate through the the json object and load the number of users that will be loading
+        //Generate random numbers to make users random
+        let randomHorses = [];
+        while(randomHorses.length < this.props.noOfHorses){
+            const random = Math.floor(Math.random()*29) + 1;
+            //If number is unique, add to array
+            if(randomHorses.indexOf(random) === -1){
+                randomHorses.push(random);
+            }
+        }
+        //Create objects of users in json file
+        let racingHorses = [];
         for(let i =0; i< this.props.noOfHorses; i++){
+            let position = randomHorses[i];
             racingHorses[i] = {
-                name: allUsers[i]['login'],
-                avatarUrl: allUsers[i]['avatar_url']
+                name: allUsers[position]['login'],
+                avatarUrl: allUsers[position]['avatar_url']
             };
         }
         return racingHorses;
