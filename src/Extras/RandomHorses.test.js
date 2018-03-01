57d5f9b414854f8967b5e0eca369c97838392cc2
Ntuthuko Mthiyane <ntuthuko.mthiyane@SAMAC001.Home>
2018-03-01 13:26:24 +0200

    changed the lengthto check using 'toHaveLength' and added null checker

--- src/Extras/RandomHorses.test.js
+++ src/Extras/RandomHorses.test.js
@@ -2,18 +2,21 @@ import { RandomHorses } from './RandomHorses';
 
 describe('It tests Random Horses', () => {
     it('it tests horse are returned', () => {
-        const horsesArray = RandomHorses(4).length;
-        expect(horsesArray).toBe(4);
+        const horsesArray = RandomHorses(4);
+        expect(horsesArray).toHaveLength(4);
+        expect(horsesArray).no.toContain(null);
     });
 
     it('it checks if there are no duplicates', () => {
         const horsesArray = RandomHorses(9);
-
-        for(let i=0; i<horsesArray.length; i++){
-            let horse = horsesArray[i];
-            let arrayRemainder = horsesArray.slice(i+1, horsesArray.length);
-            
-            expect(arrayRemainder).not.toContain(horse);
-        };
+        
+        for(let j=0; j<100; j++) {
+            for(let i=0; i<horsesArray.length; i++){
+                let horse = horsesArray[i];
+                let arrayRemainder = horsesArray.slice(i+1, horsesArray.length);
+                
+                expect(arrayRemainder).not.toContain(horse);
+            };
+        }
     });
 });
